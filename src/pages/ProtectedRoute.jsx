70718386eb1a8a4ecd0b96070f594a2836bebebb
Pavitrank <pavitrank2002@gmail.com>
2024-07-17 21:14:31 +0530

    New Commit

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -3,15 +3,16 @@ import { useAuth } from "../contexts/FakeAuthContext";
 import { useEffect } from "react";
 
 function ProtectedRoute({ children }) {
-  const isAuthenicated = useAuth();
+  const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
   useEffect(
     function () {
-      if (!isAuthenicated) navigate("/app");
+      if (!isAuthenticated) navigate("/");
     },
-    [isAuthenicated, navigate]
+    [isAuthenticated, navigate]
   );
+  return isAuthenticated ? children : null;
 }
 
 export default ProtectedRoute;
