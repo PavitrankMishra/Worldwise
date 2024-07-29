23950aa174525d3be04898743a08cc426cb0d695
Pavitrank <pavitrank2002@gmail.com>
2024-07-29 15:14:37 +0530

    New Commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -9,13 +9,16 @@ app.use(middlewares);
 app.use("/app", apiRouter);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, '../FrontEnd/build')));
 
 app.get("/api", (req, res) => {
   res.send({ message: "Hello from the server!" });
 });
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname));
+  res.sendFile(path.join(__dirname, '../FrontEnd/build', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
