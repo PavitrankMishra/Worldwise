2974bcb5c4356f74c54e136a27cf5b3a87be8304
Pavitrank <pavitrank2002@gmail.com>
2024-07-29 15:23:36 +0530

    New Commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const path = require("path");
-const jsonServer = require("json-server");
+import express from "express";
+import { join } from "path";
+import { router, defaults } from "json-server";
 const app = express();
-const apiRouter = jsonServer.router("data/cities.json");
-const middlewares = jsonServer.defaults();
+const apiRouter = router("data/cities.json");
+const middlewares = defaults();
 
 app.use(middlewares);
 app.use("/app", apiRouter);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../FrontEnd/build')));
+app.use(join(__dirname, '../FrontEnd/build'));
 
 app.get("/api", (req, res) => {
   res.send({ message: "Hello from the server!" });
@@ -18,7 +18,7 @@ app.get("/api", (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '../FrontEnd/build', 'index.html'));
+  res.sendFile(join(__dirname, '../FrontEnd/build', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
